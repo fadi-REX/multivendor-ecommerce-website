8b07471e946a7a1c7e0870418dcdc3304e11bf12
fadi <fadi.abdessaleem@gmail.com>
2024-05-20 15:43:56 +0100

    c

--- carrivo/src/App.jsx
+++ carrivo/src/App.jsx
@@ -17,7 +17,7 @@ import AboutUs from './pages/aboutus'
 
 
 
-axios.defaults.baseURL = "http://localhost:4000"
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 axios.defaults.withCredentials = true ; // for the cookies
 
 function App() {
