4fa88e178eacb891b6201147b68b3494b71f0b64
fadi <fadi.abdessaleem@gmail.com>
2024-05-28 14:41:15 +0100

    version 0.5

--- carrivo/src/components/header.jsx
+++ carrivo/src/components/header.jsx
@@ -7,7 +7,7 @@ import { useContext } from "react"
   const {user}= useContext(Usercontext)
 
    return(
-     <header className='p-3 flex justify-between   w-full z-10 shadow-sm sticky top-0 '>
+     <header className='p-3 flex justify-between   w-full z-10 shadow-sm top-0 '>
         <Link to={'/'} className='flex items-center gap-1' >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8  h-8 ">
            <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" />
